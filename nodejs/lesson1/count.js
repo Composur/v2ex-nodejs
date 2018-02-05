25625cf9928c18e11415caec4f2481f73a5d3d26
Composur <xutongxw@163.com>
2018-02-05 14:36:39 +0800

    "add nodejs pipe"

---
+++ nodejs/lesson1/count.js
@@ -0,0 +1,15 @@
+var counter=function(arr){
+    return arr.length
+}
+var adder=function(a,b){
+    return `the sum is ${a+b}`
+}
+// module.exports.counter=counter
+
+// module.exports.adder=adder
+
+
+// module.exports={
+//     counter:counter,
+//     adder:adder,
+// }
