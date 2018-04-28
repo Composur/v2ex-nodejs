80450079d3589c98ce167bd0b71ded6eaacbd005
Composur <xutongxw@163.com>
2018-04-28 18:45:53 +0800

    ‘增加注释’

--- nodejs/stickyNotes/app.js
+++ nodejs/stickyNotes/app.js
@@ -13,10 +13,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// 中间件
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// 静态资源
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
