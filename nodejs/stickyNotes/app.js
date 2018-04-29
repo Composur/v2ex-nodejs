cfb0275ccc6ad5b569ab1803f12967c66fa0e58c
Composur <xutongxw@163.com>
2018-04-29 22:01:12 +0800

    "test全局中间件userInfo"

--- nodejs/stickyNotes/app.js
+++ nodejs/stickyNotes/app.js
@@ -24,6 +24,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+// 设置全局的userInfo
+// app.use(function(req,res,next){
+//   req.userInfo={'title':"test"}
+// })
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- nodejs/stickyNotes/routes/index.js
+++ nodejs/stickyNotes/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'StickyNotes' });
-});
-
+  // res.render('index', { title: 'StickyNotes' });
+  res.render('index', { title:'stickNotes' })
+})
 module.exports = router;
