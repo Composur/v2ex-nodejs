793f9a73bd289d19fea5e13dad44491953cfa572
Composur <xutongxw@163.com>
2018-05-10 10:26:49 +0800

    ‘可以查询首页的所有数据’

--- nodejs/stickyNotes/routes/api.js
+++ nodejs/stickyNotes/routes/api.js
@@ -2,7 +2,7 @@ var express=require('express')
 var router=express.Router()
 var Note=require('../model/note').Note
 
-
+// 首页加载查询所有
 router.get('/notes',function(req,res,next){
     var note=req.body.note
     Note.findAll({raw:true}).then(data=>{
@@ -13,13 +13,13 @@ router.get('/notes',function(req,res,next){
     })
 })
 
-
-router.get('/notes',function(req,res,next){
-        console.log('notes')
-})
-
 router.post('/notes/add',function(req,res,next){
-   console.log('test')
+    var note=req.body.note
+    Note.create({text:note}).then(function(){
+        res.send({status:0})
+    }).catch(function(){
+        res.send({status:1,errorMsg:'数据库出错！'})
+    })
 })
 
 router.get('/notes/del',function(req,res,next){
