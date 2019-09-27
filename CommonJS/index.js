c5bedada2bae3b4a928d7cdd9569d6efab31c69c
composure <xutong1107@gamil.com>
2019-09-27 10:43:54 +0800

    update:加入process.stdin

--- CommonJS/index.js
+++ CommonJS/index.js
@@ -1,10 +1,19 @@
-const result=require('./lib/common')
+const playAction=require('./lib/common')
 
 // 获取nodejs（第一个）输入的变量
 const userAction = process.argv[process.argv.length - 1]
 
 // 让程序不中断，一直执行
+let result=0;
 process.stdin.on('data',e=>{
   const userInputValue=e.toString().trim()
-  result(userInputValue)
+  result= playAction(userInputValue)
+  if(result==-1){
+    result++
+    console.log(result)
+  }
+  if(result===3){
+    console.log('你太厉害了！！')
+    process.exit()
+  }
 })
--- CommonJS/lib/common.js
+++ CommonJS/lib/common.js
@@ -1,26 +1,29 @@
-module.exports=function(userAction){
-const {log}=console
+module.exports = function (userAction) {
+  const { log } = console
   // 计算机随机出一个
-let random = Math.random() * 3
-let computerAction;
-const rock = 'rock', paper = 'paper', scissor = 'scissor';
-if (random < 1) {
-  computerAction = rock
-} else if (random > 2) {
-  computerAction = paper
-} else {
-  computerAction = scissor
-}
-log(`我出了${userAction}；计算机出了${computerAction}`)
-if (userAction === computerAction) {
-  log('平局！！')
-} else if (
-  (userAction === rock && computerAction == scissor) ||
-  (userAction === paper && computerAction == rock) ||
-  (userAction === scissor && computerAction == paper)
-) {
-  log('你赢了！！')
-} else {
-  log('你输了！！')
-}
+  let random = Math.random() * 3
+  let computerAction;
+  const rock = 'rock', paper = 'paper', scissor = 'scissor';
+  if (random < 1) {
+    computerAction = rock
+  } else if (random > 2) {
+    computerAction = paper
+  } else {
+    computerAction = scissor
+  }
+  log(`你出了${userAction}；计算机出了${computerAction}`)
+  if (userAction === computerAction) {
+    log('平局！！')
+    return 0
+  } else if (
+    (userAction === rock && computerAction == scissor) ||
+    (userAction === paper && computerAction == rock) ||
+    (userAction === scissor && computerAction == paper)
+  ) {
+    log('你赢了！！')
+    return -1
+  } else {
+    log('你输了！！')
+    return 1
+  }
 }
